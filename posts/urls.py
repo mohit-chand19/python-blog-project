07665f279c5355c38a09cc0eace8c6c7765ddfc0
mohit-chand19 <chandmohit002@gmail.com>
2025-01-28 11:41:10 +0545

    next commit

--- posts/urls.py
+++ posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path('post/<str:pk>',views.post, name='post'),
 ]
